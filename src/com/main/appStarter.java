d3e1aed4ddcd60447d12948b73180d3b83e93bd9
HiromuG <asas34520@gmail.com>
2021-12-28 20:29:42 +0800

    123

--- src/com/main/appStarter.java
+++ src/com/main/appStarter.java
@@ -6,6 +6,7 @@ public class appStarter {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("?");
+		System.out.println("!?!?!?!?!?!!?!?!?!?!?!?!??!");
 	}
 
 }
